6ed8b5d4db9aaa8dcd9072a0beaa1d9c1cb53e33
Alexandre Gràcia <alexusgracia@users.noreply.github.com>
2025-03-20 15:51:32 +0100

    excel settings

--- src/app.py
+++ src/app.py
@@ -36,6 +36,7 @@ def static_files(filename):
 @app.route("/submit_new_point", methods=["GET", "POST"])
 def submit_new_point():
     if request.method == "POST":
+
         # Obtener los datos del formulario
         new_point = {
             "id": request.form.get("id"),
@@ -78,8 +79,8 @@ def submit_new_point():
         data.append(new_point)
 
         # Guardar en el archivo JSON
-        with open(data_file, "w") as f:
-            json.dump(data, f, indent=4)
+        with open(data_file, "w") as file:
+            json.dump(data, file, indent=4)
 
         return redirect(url_for("index"))
 
@@ -90,18 +91,66 @@ def submit_new_point():
 def download_excel():
 
 
-    # Convertir los datos a un DataFrame (asegúrate de que 'data' sea una lista de diccionarios)
+    # Convertir los datos a un DataFrame
     df = pd.DataFrame(data)
     output = BytesIO()
+
+    columns = [
+        'country', 'Region_state', 'last_observation', 'europe', 'repetitive',
+        'trigger', 'duration', 'frequency', 'involved_devices', 'propagation',
+        'distance', 'radius', 'outages', 'identification', 'method', 'purported',
+        'mitigation_measures','first_observation', 'grid', 'id'
+    ]
+    excluded = [
+        'coordinates', 'images', 'links',  'points', 'connections'
+    ]
+    df = df[columns]
+
 
     # Escribir el DataFrame a un archivo Excel en memoria
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='Oscillations')
+
+        # Obtener la hoja de trabajo (worksheet)
+        workbook = writer.book
+        worksheet = writer.sheets['Oscillations']
+
+        # Crear formato con color de fondo PANTONE 3005
+        header_format = workbook.add_format({
+            'bold': True,               # Negrita
+            'font_color': 'white',       # Texto blanco
+            'bg_color': '#0077C8',       # Azul PANTONE 3005
+            'align': 'center',           # Centrado horizontal
+            'valign': 'vcenter',         # Centrado vertical
+            'border': 1                  # Borde delgado
+        })
+
+        # Aplicar el formato a la primera fila (encabezados)
+        for i, col in enumerate(df.columns):
+            content_max_length = df[col].astype(str).apply(len).max() + 1  # Longitud máxima del contenido + margen
+            title_length = len(col) + 6  # Longitud del título + margen adicional si es más largo
+            optimal_width = max(content_max_length, title_length)  # Elegir el más largo entre contenido y título
+            worksheet.set_column(i, i, optimal_width)
+            worksheet.write(0, i, col.capitalize(), header_format)  # Aplicar formato al titulo y primera letra en mayúscula
+
+
+        # Ajustar la altura de la primera fila para que el filtro no tape el texto
+        worksheet.set_row(0, 20)  # Ajusta la altura de la primera fila
+
+        # Aplicar filtro a la primera fila (desde A1 hasta la última columna con datos)
+        worksheet.autofilter(0, 0, 0, len(columns) - 1)
+
+        # Bloquear la primera fila y las 3 primeras columnas
+        worksheet.freeze_panes(1, 3)
+
     output.seek(0)
+
+
 
     # Obtener fecha y hora actuales y formatearlas
     now = datetime.now().strftime("%Y%m%d")
     filename = f"oscillations_{now}.xlsx"
+
     # Enviar el archivo Excel como descarga
     return send_file(
         output,
@@ -109,6 +158,7 @@ def download_excel():
         download_name=filename,
         mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
 if __name__ == "__main__":
     port_flask = int(os.getenv("FLASK_RUN_PORT", 8080))  # Toma el puerto del .env o usa 8080 por defecto
     host_flask = os.getenv("FLASK_RUN_HOST", "0.0.0.0")  # Toma el host del .env o usa 0.0.0.0 por defecto
